499c98539d47375532015b0d001d9f06adb01bcb
Plorial <lanshunoda@rambler.ru>
2016-07-02 17:42:16 +0300

    connecting to cloud storage

--- src/main/java/com/plorial/subsdownloader/Main.java
+++ src/main/java/com/plorial/subsdownloader/Main.java
@@ -2,14 +2,28 @@ package com.plorial.subsdownloader;
 
 import com.github.wtekiela.opensub4j.api.OpenSubtitles;
 import com.github.wtekiela.opensub4j.impl.OpenSubtitlesImpl;
-import com.github.wtekiela.opensub4j.response.ServerInfo;
-import com.github.wtekiela.opensub4j.response.SubtitleFile;
 import com.github.wtekiela.opensub4j.response.SubtitleInfo;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.model.Bucket;
+import com.google.api.services.storage.model.ObjectAccessControl;
+import com.google.api.services.storage.model.Objects;
+import com.google.api.services.storage.model.StorageObject;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
 import org.apache.xmlrpc.XmlRpcException;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by plorial on 6/30/16.
@@ -21,10 +35,35 @@ public class Main {
     private static final String USER_AGENT = "OSTestUserAgent";
     private static final String URL = "http://api.opensubtitles.org/xml-rpc";
 
-    public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+    public static void main(String[] args) throws MalformedURLException, XmlRpcException, InterruptedException {
         URL url = new URL(URL);
         OpenSubtitles subtitles = new OpenSubtitlesImpl(url);
         subtitles.login(LOGIN,PASS,"eng",USER_AGENT);
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setServiceAccount(new FileInputStream("/home/plorial/Documents/Exoro Player-96d81f15e21c.json"))
+                    .setDatabaseUrl("https://exoro-player.firebaseio.com/")
+                    .build();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        FirebaseApp.initializeApp(options);
+
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference("Series");
+
+        try {
+            Bucket list = getBucket("exoro-player.appspot.com");
+
+            System.out.println(list.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
 
         Map<Integer,  Integer> seasons= new HashMap<>();
         seasons.put(1,10);
@@ -34,7 +73,38 @@ public class Main {
         seasons.put(5,10);
         seasons.put(6,10);
 
-        downloadSerial("Game of Thrones",seasons,subtitles);
+//        downloadSerial("Game of Thrones",seasons,subtitles);
+
+    }
+
+    public static List<StorageObject> listBucket(String bucketName)
+            throws IOException, GeneralSecurityException {
+        Storage client = StorageFactory.getService();
+        Storage.Objects.List listRequest = client.objects().list(bucketName);
+
+        List<StorageObject> results = new ArrayList<StorageObject>();
+        Objects objects;
+
+        // Iterate through each page of results, and add them to our results list.
+        do {
+            objects = listRequest.execute();
+            // Add the items in this page of results to the list we'll return.
+            results.addAll(objects.getItems());
+
+            // Get the next page, in the next iteration of this loop.
+            listRequest.setPageToken(objects.getNextPageToken());
+        } while (null != objects.getNextPageToken());
+
+        return results;
+    }
+
+    public static Bucket getBucket(String bucketName) throws IOException, GeneralSecurityException {
+        Storage client = StorageFactory.getService();
+
+        Storage.Buckets.Get bucketRequest = client.buckets().get(bucketName);
+        // Fetch the full set of the bucket's properties (e.g. include the ACLs in the response)
+        bucketRequest.setProjection("full");
+        return bucketRequest.execute();
     }
 
     private static void downloadSerial(String serial, Map<Integer, Integer> seasons, OpenSubtitles subtitles){
