2abb573aebf84c7166fc1f76efc87e5313d2776b
Plorial <lanshunoda@rambler.ru>
2016-07-08 16:29:59 +0300

    5 accs login

--- src/main/java/com/plorial/subsdownloader/Main.java
+++ src/main/java/com/plorial/subsdownloader/Main.java
@@ -26,21 +26,24 @@ import java.util.Map;
  */
 public class Main {
 
-    private static final String LOGIN = "Plorial";
-    private static final transient String PASS = "qazwsx";
-    private static final String USER_AGENT = "OSTestUserAgent";
+    private static final String[] LOGIN = {"plorial","lanshunoda", "shport93", "lan_yandex", "lan_yahoo"};
+    private static final String[] PASS = {"qazwsx","v9akueyp", "qazwsx","qazwsx","qazwsx" };
+    private static final String USER_AGENT = "plorial";
     private static final String URL = "http://api.opensubtitles.org/xml-rpc";
 
     public static final String BUCKET = "exoro-player.appspot.com";
 
-    private static int counter = 0;
+    private static int download_counter = 0;
+    private static int login_counter = 0;
+    private static OpenSubtitles subtitles;
 
     private static Storage storage;
     private static DatabaseReference dbRef;
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         URL url = new URL(URL);
-        OpenSubtitles subtitles = new OpenSubtitlesImpl(url);
+
+        subtitles = new OpenSubtitlesImpl(url);
 
         FirebaseOptions options = null;
         try {
@@ -66,16 +69,15 @@ public class Main {
         }
 
         Map<Integer,  Integer> seasons= new HashMap<>();
-//        seasons.put(1,10);
-//        seasons.put(2,10);
-        seasons.put(3,10);
-        seasons.put(4,10);
-        seasons.put(5,10);
-        seasons.put(6,10);
+        seasons.put(1,7);
+        seasons.put(2,13);
+        seasons.put(3,13);
+        seasons.put(4,13);
+        seasons.put(5,16);
 
         try {
-            subtitles.login(LOGIN,PASS,"eng",USER_AGENT);
-            downloadSerial("Game of Thrones",seasons,subtitles);
+            subtitles.login(LOGIN[login_counter],PASS[login_counter],"eng",USER_AGENT);
+            downloadSerial("Firefly",seasons);
         } catch (XmlRpcException e) {
             e.printStackTrace();
         }
@@ -109,7 +111,7 @@ public class Main {
         return null;
     }
 
-    private static void downloadSerial(String serial, Map<Integer, Integer> seasons, OpenSubtitles subtitles) throws XmlRpcException {
+    private static void downloadSerial(String serial, Map<Integer, Integer> seasons) throws XmlRpcException {
         for(Map.Entry<Integer,Integer> entry : seasons.entrySet()) {
             int episodes = entry.getValue();
             for(int j = 1; j < episodes+1; j++) {
@@ -129,8 +131,15 @@ public class Main {
                             String subFile = subtitleFile.get(0).getContentAsString(subs.get(0).getSubEncoding());
                             String filePath = Downloader.writeStringToFile(subFile, "/home/plorial/Documents/Exoro/" + serial + "/Season " +  entry.getKey() + "/Episode " + j, subs.get(0).getLanguage() + "." + subs.get(0).getFormat());
                             unzipedFiles.add(filePath);
-                            counter++;
-                            System.out.println("downloaded " + counter);
+                            download_counter++;
+                            System.out.println("downloaded " + download_counter);
+                            if(download_counter == 199 ){
+                                login_counter++;
+                                if(login_counter == 5){return;}
+                                subtitles.logout();
+                                subtitles.login(LOGIN[login_counter],PASS[login_counter],"eng",USER_AGENT);
+                                download_counter = 0;
+                            }
                         } else {
                             System.err.println("failed to load");
                         }
